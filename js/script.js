45f750fa3e63eee43394136ba6ff02a686961d19
yyyen93 <howard.wfn@gmail.com>
2023-10-23 14:37:38 +0800

    Changes commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const doms = {
     menu: document.querySelector('.hamburger-menu-links'),
@@ -9,19 +8,3 @@ function toggleMenu(){
     doms.menu.classList.toggle('open')
     doms.icon.classList.toggle('open')
 }
-=======
-const doms = {
-    icon: document.querySelector('.js--icon-hamburger'),
-    nav: document.querySelector('.js--main-nav')
-}
-
-
-doms.icon.addEventListener('click', () => {
-    doms.nav.slideToggle();
-    if (doms.nav.style.display === 'none' || doms.nav.style.display === '') {
-        doms.nav.style.display = 'block';
-    } else {
-        doms.nav.style.display = 'none';
-    }
-})
->>>>>>> fcb65c79020c9b1e04476809712f6f3a67fc6d93
